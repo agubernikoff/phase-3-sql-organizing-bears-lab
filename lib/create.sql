90e0e4de0a01a25a470e90ab2e843677f604d7d9
alexander gubernikoff <agubernikoff@gmail.com>
2022-03-16 20:55:00 -0400

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE bears(
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate',1,'M','Black','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy',2,'M','Black','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha',3,'F','Brown','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown',5,'M','Black','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa',11,'F','White','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch',3,'M','Black','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy',14,'F','Brown','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL,18,'M','Black','happy','true');
